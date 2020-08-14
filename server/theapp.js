3bce75945f09651aa6f95467fffc9869ab72f846
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-08-14 04:44:22 +0100

    distances

--- server/theapp.js
+++ server/theapp.js
@@ -7,6 +7,15 @@ const fs = require('fs')
 const results = [];
  
 
+var fs2 = require('fs');
+
+// file is included here:
+eval(fs2.readFileSync('./public/geotools.js')+'');
+
+var fs3 = require('fs');
+eval(fs3.readFileSync('./public/my-haversine.js')+'');
+
+
 
 fs.createReadStream('data.csv')
   .pipe(csv())
@@ -53,16 +62,44 @@ theapp.get("/files", (req,res,next) => {
     
 });
 
-theapp.get("/data",(req,res,next) =>{
+theapp.get("/data/:postcode",(req,res,next) =>{
 
-    results.length = 0;
+    //results.length = 0;
+
+    var theData = [];
 
     fs.createReadStream('./public/registered-raw-drinking-milk-producers-as-at-1-august-2020.csv')
         .pipe(csv())
-        .on('data', (data) => results.push(data))
+        .on('data', (data) => {
+
+            var sPC = req.params['postcode'].split(' ').join('').toUpperCase();
+            var ePC = data['PostCode'].split(' ').join('').toUpperCase();
+
+            if((sPC in postcode_BGC) && (ePC in postcode_BGC)){
+                //create a osgb coordinate 
+                osgb=new GT_OSGB(); 
+                osgb.setGridCoordinates(postcode_BGC[sPC][0], postcode_BGC[sPC][1]); 
+                wgs84 = osgb.getWGS84(); 
+
+                osgb2=new GT_OSGB(); 
+                console.log(ePC);
+                osgb2.setGridCoordinates(postcode_BGC[ePC][0], postcode_BGC[ePC][1]); 
+                wgs842 = osgb2.getWGS84();
+
+                data['distance'] =  distance =  myHaversine([wgs84.longitude,wgs84.latitude],[wgs842.longitude,wgs842.latitude]);
+
+
+                theData.push(data);
+                console.log(data);
+            }
+            else{//the post codes grid can't be found
+                data['distance'] = '';
+            }
+            
+        })
         .on('end', () => {
             //console.log(results);
-            res.json(results);
+            res.json(theData);
         });
 
 });
