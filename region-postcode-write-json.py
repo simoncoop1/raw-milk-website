ffd6821158839eaf40111df47aaa12db18c6fc16
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-08-18 15:44:39 +0100

    region

---
+++ region-postcode-write-json.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import argparse
+import sys
+import  json
+from datetime import datetime
+
+##iterate all records in the data
+def dGen():
+    current = 0
+    fileno = 0
+
+    f = "uk-counties-to-regions.csv"
+
+    with open(f, newline='') as csvfile:
+        areader = csv.reader(csvfile, delimiter=',',doublequote=False)
+        #print(f)
+
+        for row in areader:
+            #count+=1
+            #print(count)
+            yield row
+
+def go():
+    #stuff
+    out = {}
+
+    for a in dGen():
+        out[a[0]] = a[1]
+
+    with open('region-postcode.json', 'w', encoding='utf-8') as f:
+            json.dump(out, f,)
+
+
+go()
