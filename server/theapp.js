9b94ef6f2879e98d00d0650f5b6e73204ea1abcc
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-08-19 04:51:22 +0100

    Add files via upload

--- server/theapp.js
+++ server/theapp.js
@@ -29,6 +29,7 @@ fs.createReadStream('data.csv')
   });
 
 var postcode_BGC = require('./postcode-BGC.json');
+var region_postcode = require('./region-postcode.json');
 
 theapp.get("/url", (req, res, next) => {
     res.json(["this","is","my","test","server"]);
@@ -66,43 +67,123 @@ theapp.get("/data/:postcode",(req,res,next) =>{
 
     //results.length = 0;
 
-    var theData = [];
+    var theData = [];//build json for response
+    var sPC = req.params['postcode'].split(' ').join('').toUpperCase(); // the start postcode
 
     fs.createReadStream('./public/registered-raw-drinking-milk-producers-as-at-1-august-2020.csv')
         .pipe(csv())
-        .on('data', (data) => {
-
-            var sPC = req.params['postcode'].split(' ').join('').toUpperCase();
-            var ePC = data['PostCode'].split(' ').join('').toUpperCase();
-
-            if((sPC in postcode_BGC) && (ePC in postcode_BGC)){
-                //create a osgb coordinate 
-                osgb=new GT_OSGB(); 
-                osgb.setGridCoordinates(postcode_BGC[sPC][0], postcode_BGC[sPC][1]); 
-                wgs84 = osgb.getWGS84(); 
-
-                osgb2=new GT_OSGB(); 
-                console.log(ePC);
-                osgb2.setGridCoordinates(postcode_BGC[ePC][0], postcode_BGC[ePC][1]); 
-                wgs842 = osgb2.getWGS84();
-
-                data['distance'] =  distance =  myHaversine([wgs84.longitude,wgs84.latitude],[wgs842.longitude,wgs842.latitude]);
+        .on('data', function(data){addDistance(data,sPC,theData);})
+        .on('end', () => {
+            //console.log(results);
+            res.json(theData);
+        });
+});
 
 
-                theData.push(data);
-                console.log(data);
-            }
-            else{//the post codes grid can't be found
-                data['distance'] = '';
-            }
+theapp.get("/data/:postcode/:animals/:rating/:region",(req,res,next) =>{
+
+//res.json([req.params['animals'],req.params['rating'],req.params['region']]);
+
+
+    var theData = [];//build json for response
+    var sPC = req.params['postcode'].split(' ').join('').toUpperCase(); // the start postcode
+
+    fs.createReadStream('./public/registered-raw-drinking-milk-producers-as-at-1-august-2020.csv')
+        .pipe(csv())
+        .on('data', function(data){
+
+            //filter out only add distance to those that passed filters
+            //on stuff reaching addDistance in added
+            if ((req.params['animals'].indexOf("cows") > -1 & data['HasCows'] == 'YES')
+                || req.params['animals'].indexOf("all") > -1
+                || (req.params['animals'].indexOf("goats") > -1 & data['HasGoats'] == 'YES')
+                || (req.params['animals'].indexOf("horses") > -1 & data['HasHorses'] == 'YES')
+                || (req.params['animals'].indexOf("sheep") > -1 & data['HasSheep'] == 'YES')
+                || (req.params['animals'].indexOf("buffalo") > -1 & data['HasBuffalo'] == 'YES')){
             
-        })
+                if((req.params['rating'].indexOf("good") > -1 & data['ComplianceRating'] == 'GOOD')
+                    || (req.params['rating'].indexOf("generally satisfactory") > -1 & data['ComplianceRating'] == 'GENERALLY SATISFACTORY')
+                    || containsParam('all', req.params['rating'] )){
+                    if(req.params['region'].indexOf("all") > -1 
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east of england") > -1 & addressIn("East of",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                        || (req.params['region'].indexOf("east midlands") > -1 & addressIn("East Midlands",data))
+                    ){
+                        if(data['ComplianceRating']== 'GENERALLY SATISFACTORY')
+                        {
+                            console.log("yes");
+                        }
+                        addDistance(data,sPC,theData);
+
+                    }
+
+
+                }
+
+
+
+
+        }})
         .on('end', () => {
             //console.log(results);
             res.json(theData);
         });
 
+
 });
+
+
+//parameters are comma separating e.g cow,goat,
+//function check if a parameter is present
+function containsParam(p, paramstr){
+    var ar = paramstr.split(',')
+    for (const a  of ar){
+            if(p == a){
+                return true
+            }
+    }
+    return false
+}
+
+function addDistance(dataIn, sPC, theDataOut){
+
+    //var sPC = req.params['postcode'].split(' ').join('').toUpperCase();
+    var ePC = dataIn['PostCode'].split(' ').join('').toUpperCase();
+
+    if((sPC in postcode_BGC) && (ePC in postcode_BGC)){
+        //create a osgb coordinate 
+        osgb=new GT_OSGB();
+        osgb.setGridCoordinates(postcode_BGC[sPC][0], postcode_BGC[sPC][1]);
+        wgs84 = osgb.getWGS84();
+
+        osgb2=new GT_OSGB();
+        //console.log(ePC);
+        osgb2.setGridCoordinates(postcode_BGC[ePC][0], postcode_BGC[ePC][1]);
+        wgs842 = osgb2.getWGS84();
+
+        dataIn['distance'] =  distance =  myHaversine([wgs84.longitude,wgs84.latitude],[wgs842.longitude,wgs842.latitude]);
+
+        theDataOut.push(dataIn);
+
+//        console.log(dataIn);
+    }
+    else{//the post codes grid can't be found
+        dataIn['distance'] = '';
+    }
+}
+
+function addressIn(a, b){
+    return false;
+}
 
 theapp.listen(3003, () => {
          console.log("Server running on port 3003");
