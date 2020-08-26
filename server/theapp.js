f8c6b2d528dd6bff4f1c4f8b6e6924fe37a4d1c7
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-08-26 16:16:07 +0100

    suggestion

--- server/theapp.js
+++ server/theapp.js
@@ -14,6 +14,8 @@ eval(fs2.readFileSync('./public/geotools.js')+'');
 var fs3 = require('fs');
 eval(fs3.readFileSync('./public/my-haversine.js')+'');
 
+const d3 = require('d3-array');
+
 fs.createReadStream('data.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
@@ -28,7 +30,8 @@ fs.createReadStream('data.csv')
 var postcode_BGC = require('./postcode-BGC.json');
 var region_postcode = require('./region-postcode.json');
 //alphabetic sorted postcodes
-var postcode_info = require('./postcode-info.json')
+var postcode_info = require('./postcode-info.json');
+var postcode_frame = require('./postcode-frame.json');
 
 theapp.get("/url", (req, res, next) => {
     res.json(["this","is","my","test","server"]);
@@ -117,13 +120,75 @@ theapp.get("/data/:postcode/:animals/:rating/:region",(req,res,next) =>{
 theapp.get("/suggestion/:code",(req,res,next) =>{
     //results.length = 0;
     var theData = [];//build json for response
-    var sPC = req.params['postcode'].split(' ').join('').toUpperCase(); // the start postcode
+    var sPC = req.params['code'].split(' ').join('').toUpperCase(); // the start postcode
 
-    for (const [key, value] of Object.entries(postcode_BGC)) {
-                  
+    //postcode_frame.sort(function (a, b) {
+    //        return a.replace(/\s/g, '') - b.replace(/\s/g, '');
+    //});
+    //console.log("theFirst:"+postcode_frame[0]);
+
+    //binarySearch(postcode_frame,'CV478JY');
+    //
+    s = getSuggest(postcode_frame,sPC);
+    //console.log(s);
+    res.json(s);
+           
+    
+
+});
+
+function binarySearch(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+    console.log(arr[mid], i);
+    
+    if (arr[mid] === i) {
+        console.log('match', arr[mid], i);
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length > 1) {
+        console.log('mid lower', arr[mid], i);
+        return binarySearch(arr.slice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length > 1) {
+        console.log('mid higher', arr[mid], i);
+        return binarySearch(arr.slice(0, mid), i);
+    } else {
+        console.log('not here', i);
+        return -1;
     }
     
-});
+}
+
+//used bisec to find index on ordered array.
+function getSuggest(arr, x){
+    var indx = d3.bisectLeft(arr,x);
+
+    if(arr[indx].substring(0,x.length) == x){
+        return [arr[indx]];
+    }
+    else
+        return [];
+
+}
+
+
+function binarySearchOrig(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+    console.log(arr[mid], i);
+    
+    if (arr[mid] === i) {
+        console.log('match', arr[mid], i);
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length > 1) {
+        console.log('mid lower', arr[mid], i);
+        return binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length > 1) {
+        console.log('mid higher', arr[mid], i);
+        return binarySearch(arr.splice(0, mid), i);
+    } else {
+        console.log('not here', i);
+        return -1;
+    }
+    
+}
 
 //parameters are comma separating e.g cow,goat,
 //function check if a parameter is present
